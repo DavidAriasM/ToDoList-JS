a828b913edef94447f9fb916a246fcaa507cb668
“David” <arias.maulen@gmail.com>
2022-04-12 17:08:09 -0400

    Correcciones y mejoras de responsividad

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inputTarea = document.getElementById('inputTarea');
-
+let imgVacio = document.getElementById('img_vacio');
 let dataFormulario = [];
 let contadorId = 0;
 const ingresarTarea = e => {
@@ -19,15 +19,22 @@ const ingresarTarea = e => {
 
 function mostrarData() {
     console.log(localStorage.getItem('dataFormulario'));
+    console.log(localStorage.getItem('dataFormulario').length);
     if (localStorage.getItem('dataFormulario')) {
-        let lista = document.querySelector('ul');
+
+        let lista = document.querySelector('#lista');
         lista.innerHTML = "";
         JSON.parse(localStorage.getItem('dataFormulario')).forEach(data => {
             lista.innerHTML += `
-            <li class="tarea">${ data.tarea } <button class="btnBorrar" onclick="borrarFila(${ data.id })">X</button></li>
+            <div class="tarea_contenedor"><div class="tarea">${ data.tarea }</div><button class="btnBorrar" onclick="borrarFila(${ data.id })">X</button></div>
         `
         });
+
     }
+    if (localStorage.getItem('dataFormulario').length > 2) {
+        imgVacio.style.display = 'none';
+    }
+
 }
 
 function borrarFila(id) {
@@ -39,6 +46,9 @@ function borrarFila(id) {
             dataFormulario.splice(indice, 1);
             localStorage.setItem('dataFormulario', JSON.stringify(dataFormulario));
         }
+    }
+    if (localStorage.getItem('dataFormulario').length === 2) {
+        imgVacio.style.display = 'flex';
     }
     mostrarData();
 }
